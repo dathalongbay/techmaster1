203ed6cf96154ca7d7b5f21b531cb0d2b094afb7
datdo <dathalongbay@gmail.com>
2018-08-05 19:54:58 +0700

    xoa 2 file

--- session/index.php
+++ session/index.php
@@ -20,25 +20,12 @@ session_start();
 <body>
 <div class="container">
 
-    <?php
-
-    // Set session variables
-    $_SESSION["username"] = "dat";
-    $_SESSION["address"] = "ha noi";
-    echo "Session variables are set.";
-
-
-    echo "<pre>";
-    print_r($_SESSION);
-    echo "</pre>";
-    ?>
-
-    <form class="form-signin" style="margin: 0 auto; width: 500px">
+    <form name="login" action="http://localhost/techmaster1//session/admin.php" class="form-signin" style="margin: 0 auto; width: 500px" method="post">
         <h2 class="form-signin-heading">Please sign in</h2>
         <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
         <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
         <div class="checkbox">
             <label>
                 <input type="checkbox" value="remember-me"> Remember me
